be919da0dfd00de0c7f8f0f75f0ee52bb058c563
seien2828 <kuma.fantasista@gmail.com>
2018-11-22 00:28:21 +0900

    create

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>HTML5サンプル</title>
+<link rel="stylesheet" href"stile.css" >
+</head>
+<body>
+<h1>テスト</h1>
+<p>HTML5で作成しました！</p>
+<?php$gundam="00ガンダム";?>
+<P>現環境最強機体は <?php echo $gundam; ?>です。</p>
+</body>
+</html>
